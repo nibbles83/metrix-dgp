6b90eed370b61eb61e0d552ddd5801b536b1b75b
David Stoneham <dstoneham.93@gmail.com>
2020-03-05 21:44:19 +1000

    allow mulitple contract deployment add custom contract deployment

--- test/helpers.js
+++ test/helpers.js
@@ -3,53 +3,92 @@ const { Qtum } = require("qtumjs")
 const solc = require('solc')
 const fs = require('fs-extra')
 const path = require('path')
+const createKeccakHash = require('keccak')
 
 const rpcURL = "http://user:pass@localhost:13889";
 let qtum = new Qtum(rpcURL);
 let mainAddress = "";
 let mainAddressHex = "";
-let contract;
 
 /*
 Contract deployment functions
 */
-async function deploy(name) {
-    let builtContract = buildContract(name);
-    builtContract = builtContract.contracts[name][name.split(".")[0]]
-    let deployment = await deployContract(name, builtContract);
-
+async function deploy(contractNames) {
     let solar = {
         "contracts": {
-            [name]: {
-                abi: builtContract.abi,
-                "bin": builtContract.evm.bytecode.object,
-                "binhash": "",
-                "name": name.split(".")[0],
-                "deployName": name,
-                "address": deployment.address,
-                "txid": deployment.txid,
-                "createdAt": new Date(),
-                "confirmed": true,
-                "sender": deployment.sender,
-                "senderHex": deployment.hash160
-            }
         }
     }
+    let testData = {
+        gasScheduleAddress: '',
+        blockSizeAddress: '',
+        minGasPriceAddress: '',
+        blockGasLimitAddress: '',
+        governanceCollateralAddress: '',
+        budgetFeeAddress: '',
+        DGPAddress: ''
+    }
+    for (let i = 0; i < contractNames.length; i++) {
+        let name = contractNames[i];
+        let builtContract = buildContract(name, testData);
+        builtContract = builtContract.contracts[name][name.split(".")[0].split("-")[0]]
+        let deployment = await deployContract(name, builtContract);
 
-    mainAddress = deployment.sender;
-    mainAddressHex = deployment.hash160;
+        solar.contracts[name] = {
+            abi: builtContract.abi,
+            "bin": builtContract.evm.bytecode.object,
+            "binhash": "",
+            "name": name.split(".")[0],
+            "deployName": name,
+            "address": deployment.address,
+            "txid": deployment.txid,
+            "createdAt": new Date(),
+            "confirmed": true,
+            "sender": deployment.sender,
+            "senderHex": deployment.hash160
+        }
+
+        if (i === 0) {
+            mainAddress = deployment.sender;
+            mainAddressHex = deployment.hash160;
+        }
+
+        if (name === 'gasSchedule-dgp.sol') testData.gasScheduleAddress = toChecksumAddress(deployment.address)
+        if (name === 'blockSize-dgp.sol') testData.blockSizeAddress = toChecksumAddress(deployment.address)
+        if (name === 'minGasPrice-dgp.sol') testData.minGasPriceAddress = toChecksumAddress(deployment.address)
+        if (name === 'blockGasLimit-dgp.sol') testData.blockGasLimitAddress = toChecksumAddress(deployment.address)
+        if (name === 'governanceCollateral-dgp.sol') testData.governanceCollateralAddress = toChecksumAddress(deployment.address)
+        if (name === 'budgetFee-dgp.sol') testData.budgetFeeAddress = toChecksumAddress(deployment.address)
+        if (name === 'DGP.sol') testData.DGPAddress = toChecksumAddress(deployment.address)
+    }
+
     qtum = new Qtum(rpcURL, solar);
-    contract = qtum.contract(name);
 }
 
-function buildContract(name) {
+function buildContract(name, testData, customData) {
     let contract = fs.readFileSync(path.resolve(__dirname, '../', 'contracts', name), 'utf8');
 
     // set some dev variables for Governance.sol
     if (name === 'Governance.sol') {
-        contract = contract.replace("uint16 private _minimumGovernors = 100;", "uint16 private _minimumGovernors = 3;")
         contract = contract.replace("uint16 private _pingBlockInterval = 30 * 960;", "uint16 private _pingBlockInterval = 40;")
         contract = contract.replace("uint16 private _rewardBlockInterval = 2000;", "uint16 private _rewardBlockInterval = 100;")
+    } else if (name === 'DGP.sol') {
+        contract = contract.replace("uint16 private _minimumGovernors = 100;", "uint16 private _minimumGovernors = 3;")
+        contract = contract.replace("0x0000000000000000000000000000000000000087", "0x0");
+    }
+    // set contract addresses if necessary
+    if (testData.gasScheduleAddress) contract = contract.replace("0x0000000000000000000000000000000000000080", testData.gasScheduleAddress)
+    if (testData.blockSizeAddress) contract = contract.replace("0x0000000000000000000000000000000000000081", testData.blockSizeAddress)
+    if (testData.minGasPriceAddress) contract = contract.replace("0x0000000000000000000000000000000000000082", testData.minGasPriceAddress)
+    if (testData.blockGasLimitAddress) contract = contract.replace("0x0000000000000000000000000000000000000083", testData.blockGasLimitAddress)
+    if (testData.governanceCollateralAddress) contract = contract.replace("0x0000000000000000000000000000000000000084", testData.governanceCollateralAddress)
+    if (testData.budgetFeeAddress) contract = contract.replace("0x0000000000000000000000000000000000000085", testData.budgetFeeAddress)
+    if (testData.DGPAddress) contract = contract.replace("0x0000000000000000000000000000000000000086", testData.DGPAddress)
+    if (testData.DGPAddress) contract = contract.replace("0x0000000000000000000000000000000000000086", testData.DGPAddress)
+    // set any custom data
+    if (customData) {
+        customData.forEach(item => {
+            contract = contract.replace(item[0], item[1]);
+        })
     }
 
     const getImports = (dependency) => {
@@ -72,8 +111,6 @@ function buildContract(name) {
         }
     };
 
-    //var output = JSON.parsesolc.compileStandardWrapper(JSON.stringify(input), getImports))
-
     return JSON.parse(solc.compileStandardWrapper(JSON.stringify(input), getImports))
 }
 
@@ -85,11 +122,30 @@ async function deployContract(name, contract) {
     return res;
 }
 
+function deployCustomContract(name, data) {
+    let builtContract = buildContract(name, {}, data);
+    builtContract = builtContract.contracts[name][name.split(".")[0].split("-")[0]]
+    return deployContract(name, builtContract);
+}
+
+function toChecksumAddress(address) {
+    address = address.toLowerCase();
+    var hash = createKeccakHash('keccak256').update(address).digest('hex')
+    var ret = '0x';
+    for (let i = 0; i < address.length; i++) {
+        if (parseInt(hash[i], 16) >= 8) {
+            ret += address[i].toUpperCase()
+        } else {
+            ret += address[i]
+        }
+    }
+    return ret;
+}
 
 module.exports = {
     deploy,
+    deployCustomContract,
     qtum: () => { return qtum },
     mainAddress: () => { return mainAddress },
-    mainAddressHex: () => { return mainAddressHex },
-    contract: () => { return contract }
+    mainAddressHex: () => { return mainAddressHex }
 }
