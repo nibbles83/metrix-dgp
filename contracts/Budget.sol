675b2a5e25917fe77f7297c39107c18a93a853b5
David Stoneham <dstoneham.93@gmail.com>
2020-03-09 17:46:19 +1000

    update budget contract add budget tests

--- contracts/Budget.sol
+++ contracts/Budget.sol
@@ -1,20 +1,30 @@
-pragma solidity ^0.5.8;
+pragma solidity 0.5.8;
 import "./SafeMath.sol";
 
 // Governance interface
 contract GovernanceInterface {
-    function budgetProposalFee() public returns (uint256);
-    function canVoteOnBudget(address votingAddress) public returns (bool);
+    function isValidGovernor(address governorAddress, bool checkPing)
+        public
+        view
+        returns (bool valid);
     function ping() public;
     function governorCount() public returns (uint16);
+}
+contract DGPInterface {
+    function getBudgetFee() public view returns (uint256[1] memory);
 }
 
 contract Budget {
     // imports
     using SafeMath for uint256;
 
-    // address of the governance contract
-    address private _governanceAddress;
+    // address of the external contracts
+    address private _dgpAddress = address(
+        0x0000000000000000000000000000000000000086
+    );
+    address private _governanceAddress = address(
+        0x0000000000000000000000000000000000000087
+    );
 
     // Data structures
     enum Vote {NEW, ABSTAIN, NO, YES}
@@ -30,11 +40,13 @@ contract Budget {
         mapping(address => Vote) votes;
         uint16 yesVote;
         uint16 noVote;
+        bool remove;
     }
-    // List of existing proposals
-    BudgetProposal[] public proposals;
-    // used so each proposal can be identified for voting
-    uint256 _poposalCount = 0;
+    BudgetProposal[] public proposals; // List of existing proposals
+    uint256 private _currentProposalId = 0; // used so each proposal can be identified for voting
+    uint8 private _proposalCount = 0; // number of active proposals
+    uint8 private _maximumActiveProposals = 100; // maximum number of active proposals at any time
+    uint16 private _minimumGovernors = 100; // how many governors must exist before budget can be settled
 
     // Event that will be emitted whenever a new proposal is started
     event ProposalStarted(
@@ -48,13 +60,9 @@ contract Budget {
     );
 
     // budget block details
-    uint256 _budgetStartBlock = 10;
-    uint256 _budgetPeriod = 29219;
+    uint256 private _budgetStartBlock = 10;
+    uint256 private _budgetPeriod = 29219;
     uint256 _budgetNextSettlementBlock = _budgetStartBlock.add(_budgetPeriod);
-
-    constructor(address governanceAddress) public {
-        _governanceAddress = governanceAddress;
-    }
 
     /** @dev Function to start a new proposal.
       * @param title Title of the proposal to be created
@@ -64,33 +72,53 @@ contract Budget {
       * @param duration Duration of the payments in months
       */
     function startProposal(
-        string title,
-        string description,
-        string url,
+        string memory title,
+        string memory description,
+        string memory url,
         uint256 requested,
         uint16 duration
     ) public payable {
-        GovernanceInterface governance = GovernanceInterface(
-            _governanceAddress
+        DGPInterface contractInterface = DGPInterface(_dgpAddress);
+        uint256 listingFee = contractInterface.getBudgetFee()[0];
+        // limit number of active proposals
+        require(
+            _proposalCount < _maximumActiveProposals,
+            "Maximum active proposals reached"
         );
-        uint256 listingFee = governance.budgetProposalFee();
         // must pay listing fee
         require(msg.value == listingFee, "Buget listing fee is required");
+        // must have duration and requested amount
+        require(requested > 0, "Requested amount cannot be less than 1");
+        require(duration > 0, "Requested duration cannot be less than 1");
         // create new proposal
-        _poposalCount++;
+        _currentProposalId++;
+        _proposalCount++;
         BudgetProposal memory newProp;
-        newProp.id = _poposalCount;
+        newProp.id = _currentProposalId;
         newProp.owner = msg.sender;
         newProp.title = title;
         newProp.desc = description;
         newProp.url = url;
         newProp.requested = requested;
         newProp.duration = duration;
+        newProp.durationsPaid = 0;
         newProp.yesVote = 0;
         newProp.noVote = 0;
-        proposals.push(newProp);
+        newProp.remove = false;
+        // add proposal to array
+        uint8 i;
+        bool pushProposal = true;
+        for (i = 0; i < proposals.length; i++) {
+            if (proposals[i].remove) {
+                proposals[i] = newProp;
+                pushProposal = false;
+                break;
+            }
+        }
+        if (pushProposal) proposals.push(newProp);
+        // emit event
         emit ProposalStarted(
-            _poposalCount,
+            _currentProposalId,
             msg.sender,
             title,
             description,
@@ -105,33 +133,48 @@ contract Budget {
       * @param vote the vote being cast (no, yes, abstain)
       */
     function voteForProposal(uint256 proposalId, Vote vote) public {
-        GovernanceInterface governance = GovernanceInterface(
+        GovernanceInterface governanceInterface = GovernanceInterface(
             _governanceAddress
         );
         // must be a valid governor
         require(
-            governance.canVoteOnBudget(msg.sender),
+            governanceInterface.isValidGovernor(msg.sender, true),
             "Address is not a valid governor"
         );
+        // must be a valid proposal
+        int16 proposalRawIndex = getProposalIndex(proposalId);
+        require(proposalRawIndex >= 0, "Proposal not found");
+        uint8 proposalIndex = uint8(proposalRawIndex);
         // if vote was changed remove previous
         if (
-            proposals[proposalId].votes[msg.sender] == Vote.YES &&
+            proposals[proposalIndex].votes[msg.sender] == Vote.YES &&
             vote != Vote.YES
         ) {
-            proposals[proposalId].yesVote--;
+            proposals[proposalIndex].yesVote--;
         } else if (
-            proposals[proposalId].votes[msg.sender] == Vote.NO &&
+            proposals[proposalIndex].votes[msg.sender] == Vote.NO &&
             vote != Vote.NO
         ) {
-            proposals[proposalId].noVote--;
+            proposals[proposalIndex].noVote--;
         }
         // update vote on proposal
-        if (vote == Vote.YES) proposals[proposalId].yesVote++;
-        if (vote == Vote.NO) proposals[proposalId].noVote++;
+        if (vote == Vote.YES) proposals[proposalIndex].yesVote++;
+        if (vote == Vote.NO) proposals[proposalIndex].noVote++;
         // log vote
-        proposals[proposalId].votes[msg.sender] = vote;
+        proposals[proposalIndex].votes[msg.sender] = vote;
         // update governor ping
-        governance.ping();
+        governanceInterface.ping();
+    }
+
+    /** @dev Function to get a proposal by it's id.
+      * @param proposalId Id of the proposal
+      */
+    function getProposalIndex(uint256 proposalId) public view returns (int16) {
+        uint8 i;
+        for (i = 0; i < proposals.length; i++) {
+            if (proposals[i].id == proposalId && !proposals[i].remove) return i;
+        }
+        return -1;
     }
 
     /** @dev Function to fund contract.
@@ -151,47 +194,48 @@ contract Budget {
     function settleBudget() public {
         // must be done on correct block
         require(
-            block.number == _budgetNextSettlementBlock,
+            block.number >= _budgetNextSettlementBlock,
             "Buget can only be settled in budget block"
         );
-
-        GovernanceInterface governance = GovernanceInterface(
+        // set new budget block
+        _budgetNextSettlementBlock = block.number.add(_budgetPeriod);
+        // create governance contract interface
+        GovernanceInterface governanceInterface = GovernanceInterface(
             _governanceAddress
         );
-        uint16 governorCount = governance.governorCount();
+        uint16 governorCount = governanceInterface.governorCount();
+        bool canSettleBudget = governorCount >= _minimumGovernors;
         uint16 requiredVote = governorCount / 10;
-        uint256 allocated = 0;
 
-        // iterate through all projects
-        // failed projects are removed
-        // complete projects are removed
-        // active projects that are passing are funded
-        // active projects that have completed funded are moved to complete for 1 budget cycle
-        uint256 i;
-        for (i = 0; i < proposals.length; i++) {
-            // remove failed proposals
-            if (hasVotePassed(proposals[i], requiredVote)) {
-                delete proposals[i];
-                i--;
-                break;
-            }
-            // allocate any funds for active projects
-            if (proposals[i].durationsPaid < proposals[i].duration) {
-                bool canFund = address(this).balance >=
-                    allocated.add(proposals[i].requested);
-                if (canFund) {
-                    // fund project
-                    allocated = allocated.add(proposals[i].requested);
-                    proposals[i].durationsPaid++;
-                    address(uint160(proposals[i].owner)).transfer(
-                        proposals[i].requested
-                    );
+        if (canSettleBudget) {
+            // iterate through all projects
+            // failed projects are removed
+            // complete projects are removed
+            // active projects that are passing are funded
+            // active projects that have completed funded are moved to complete for 1 budget cycle
+            uint8 i;
+            for (i = 0; i < proposals.length; i++) {
+                // skip removed proposals
+                if (proposals[i].remove) continue;
+                // remove failed proposals
+                if (!hasVotePassed(proposals[i], requiredVote)) {
+                    removeProposal(i);
+                } else {
+                    // allocate any funds for active projects
+                    if (proposals[i].durationsPaid < proposals[i].duration) {
+                        if (address(this).balance >= proposals[i].requested) {
+                            // fund project
+                            proposals[i].durationsPaid += 1;
+                            address(uint160(proposals[i].owner)).transfer(
+                                proposals[i].requested
+                            );
+                        }
+                    }
+                    // remove project if complete
+                    if (proposals[i].durationsPaid >= proposals[i].duration) {
+                        removeProposal(i);
+                    }
                 }
-            }
-            // remove project if complete
-            if (proposals[i].durationsPaid >= proposals[i].duration) {
-                delete proposals[i];
-                i--;
             }
         }
 
@@ -201,16 +245,27 @@ contract Budget {
         }
     }
 
-    function hasVotePassed(BudgetProposal proposal, uint16 requiredVote)
+    function removeProposal(uint8 index) private {
+        proposals[index].remove = true;
+        _proposalCount--;
+    }
+
+    function hasVotePassed(BudgetProposal memory proposal, uint16 requiredVote)
         private
         pure
         returns (bool)
     {
         // passing vote requires more than 10% yes
-
         if (proposal.yesVote > proposal.noVote) {
             return (proposal.yesVote - proposal.noVote) > requiredVote;
         }
         return false;
     }
+
+    /** @dev Function to return number of active proposals.
+      */
+    function proposalCount() public view returns (uint8) {
+        return _proposalCount;
+    }
+
 }
---
+++ test/budget.test.js
@@ -0,0 +1,171 @@
+
+const expect = require('chai').expect;
+const Big = require('big.js');
+const helpers = require('./helpers');
+
+let qtum, mainAddress, govContract, budgetContract;
+
+let governorAddressList = [];
+const defaultRequiredCollateral = 10;
+const budgetFee = 1;
+const satoshi = 1E8;
+
+// compile and deploy contract before tests
+before(async () => {
+    let contracts = ['governanceCollateral-dgp.sol', 'budgetFee-dgp.sol', 'DGP.sol', 'Governance.sol', 'Budget.sol']
+    await helpers.deploy(contracts);
+    qtum = helpers.qtum();
+    mainAddress = helpers.mainAddress();
+    govContract = qtum.contract('Governance.sol');
+    budgetContract = qtum.contract('Budget.sol');
+    // enroll 10 governors
+    const addresses = await qtum.rawCall("listreceivedbyaddress");
+    for (let i = 0; i < 10; i++) {
+        let tx = await govContract.send("enroll", [], { amount: defaultRequiredCollateral, senderAddress: addresses[i].address })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        governorAddressList.push(addresses[i].address)
+    }
+    await qtum.rawCall("generatetoaddress", [10, mainAddress]);
+})
+
+describe('Budget.sol', function () {
+    it('Should fail to start a proposal as the fee is not paid', async function () {
+        const tx = await budgetContract.send("startProposal", ['title', 'description', 'url', 100, 1], { amount: budgetFee - .1 })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1)
+        expect(receipt.excepted).to.equal("Revert");
+        expect(receipt.exceptedMessage).to.equal("Buget listing fee is required");
+    });
+
+    it('Should start a proposal', async function () {
+        let title = 'proposal title';
+        let desc = 'proposal description';
+        let url = 'proposal url';
+        let amount = 100;
+        let duration = 1;
+        const tx = await budgetContract.send("startProposal", [title, desc, url, amount, duration], { amount: budgetFee, gasLimit: 500000 })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1)
+        expect(receipt.excepted).to.equal("None");
+        const result = await budgetContract.call("proposals", [0]);
+        expect(result.outputs[2]).to.equal(title);
+        expect(result.outputs[3]).to.equal(desc);
+        expect(result.outputs[4]).to.equal(url);
+        expect(result.outputs[5].toNumber()).to.equal(amount);
+        expect(result.outputs[6].toNumber()).to.equal(duration);
+        expect(result.outputs[7].toNumber()).to.equal(0);
+        expect(result.outputs[8].toNumber()).to.equal(0);
+        expect(result.outputs[9].toNumber()).to.equal(0);
+    });
+
+    it('Should fail to vote as is not a governor', async function () {
+        const address = await qtum.rawCall("getnewaddress");
+        const tx = await budgetContract.send("voteForProposal", [1, 1], { senderAddress: address })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1)
+        expect(receipt.excepted).to.equal("Revert");
+        expect(receipt.exceptedMessage).to.equal("Address is not a valid governor");
+    });
+
+    it('Should fail to vote as proposal id does not exist', async function () {
+        const tx = await budgetContract.send("voteForProposal", [99, 1], { senderAddress: governorAddressList[0] })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("Revert");
+        expect(receipt.exceptedMessage).to.equal("Proposal not found");
+    });
+
+    it('Should vote no on proposal', async function () {
+        const tx = await budgetContract.send("voteForProposal", [1, 2], { senderAddress: governorAddressList[0] })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("None");
+        const proposal = await budgetContract.call("proposals", [0]);
+        expect(proposal.outputs[8].toNumber()).to.equal(0);
+        expect(proposal.outputs[9].toNumber()).to.equal(1);
+    });
+
+    it('Should change vote to abstain on proposal', async function () {
+        const tx = await budgetContract.send("voteForProposal", [1, 1], { senderAddress: governorAddressList[0] })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("None");
+        const proposal = await budgetContract.call("proposals", [0]);
+        expect(proposal.outputs[8].toNumber()).to.equal(0);
+        expect(proposal.outputs[9].toNumber()).to.equal(0);
+    });
+
+    it('Should change vote to yes on proposal', async function () {
+        const tx = await budgetContract.send("voteForProposal", [1, 3], { senderAddress: governorAddressList[0] })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("None");
+        const proposal = await budgetContract.call("proposals", [0]);
+        expect(proposal.outputs[8].toNumber()).to.equal(1);
+        expect(proposal.outputs[9].toNumber()).to.equal(0);
+    });
+
+    it('Should add funds to budget', async function () {
+        const tx = await budgetContract.send("fund", [], { amount: 100 })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("None");
+        const result = await budgetContract.call("balance");
+        const balance = Number(Big(result.outputs[0]).div(satoshi));
+        expect(balance).to.equal(101);
+    });
+
+    it('Should remove failed proposal from budget', async function () {
+        const tx = await budgetContract.send("settleBudget")
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        const receipt = await tx.confirm(1);
+        expect(receipt.excepted).to.equal("None");
+        const result = await budgetContract.call("getProposalIndex", [1]);
+        expect(result.outputs[0].toNumber()).to.equal(-1);
+    });
+
+    it('Should have no funds in budget', async function () {
+        const result = await budgetContract.call("balance");
+        const balance = Number(Big(result.outputs[0]).div(satoshi));
+        expect(balance).to.equal(0);
+    });
+
+    it('Should pass 2 proposals, fund 1 proposal and remove 1 proposal', async function () {
+        await govContract.send("ping", [], { senderAddress: governorAddressList[1] })
+        // add 3 proposals
+        const tx1 = await budgetContract.send("startProposal", ['t1', 'd1', 'u1', 100E8, 2], { amount: budgetFee, gasLimit: 500000 });
+        const tx2 = await budgetContract.send("startProposal", ['t2', 'd2', 'u2', 100E8, 2], { amount: budgetFee, gasLimit: 500000 });
+        const tx3 = await budgetContract.send("startProposal", ['t3', 'd3', 'u3', 100E8, 2], { amount: budgetFee, gasLimit: 500000 });
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        let receipts = await Promise.all([tx1.confirm(1), tx2.confirm(1), tx3.confirm(1)]);
+        // yes vote on all 3 + no vote on first
+        let txs = await Promise.all([
+            budgetContract.send("voteForProposal", [2, 3], { senderAddress: governorAddressList[0] }),
+            budgetContract.send("voteForProposal", [3, 3], { senderAddress: governorAddressList[0] }),
+            budgetContract.send("voteForProposal", [4, 3], { senderAddress: governorAddressList[0] }),
+            budgetContract.send("voteForProposal", [2, 3], { senderAddress: governorAddressList[1] }),
+            budgetContract.send("voteForProposal", [3, 3], { senderAddress: governorAddressList[1] }),
+            budgetContract.send("voteForProposal", [4, 2], { senderAddress: governorAddressList[1] }),
+        ])
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        receipts = await Promise.all([txs[0].confirm(1), txs[1].confirm(1), txs[2].confirm(1), txs[3].confirm(1)]);
+        // fund budget
+        let tx = await budgetContract.send("fund", [], { amount: 100 })
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        let receipt = await tx.confirm(1);
+        // settle
+        tx = await budgetContract.send("settleBudget",[])
+        await qtum.rawCall("generatetoaddress", [1, mainAddress]);
+        receipt = await tx.confirm(1);
+        // check proposals
+        const p1 = await budgetContract.call("proposals", [0]);
+        const p2 = await budgetContract.call("proposals", [1]);
+        const p3 = await budgetContract.call("proposals", [2]);
+        expect(p1.outputs[10]).to.equal(false);
+        expect(p1.outputs[7].toNumber()).to.equal(1);
+        expect(p2.outputs[10]).to.equal(false);
+        expect(p2.outputs[7].toNumber()).to.equal(0);
+        expect(p3.outputs[10]).to.equal(true);
+    });
+
+});
